e05e2cb3d537e3a51fbe39fc14b697f9c3289f94
wu <wu@wu.local>
2021-04-12 23:41:07 +0800

    归并排序

--- sort/归并排序.py
+++ sort/归并排序.py
@@ -5,6 +5,8 @@
 """
 归并排序用的是分治法，把一个大问题化解为k个中问题，每个中问题再化解为k个小问题，直至问题化为最小可解的问题。对这些问题求解，再不断地合并结果，直至合并完毕。
 
+归并排序可以看作一个分而治之的过程：先将整个序列分成两半，对每一半分别进行归并排序
+
 时间复杂度：
 平均：O(nlog2n),
 
@@ -23,9 +25,9 @@ def merge(left, right):
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr
-    mid = len(arr) // 2  # 即 int(len(arr)/2)
+    half = len(arr) // 2  # 即 int(len(arr)/2)
     # 归并排序前半段，再归并排序后半段
-    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
+    left, right = merge_sort(arr[:half]), merge_sort(arr[half:])
     res = merge(left, right)
     return res
 
